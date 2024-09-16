af7a7ee5e34ccc4e1e34c8bad6acfc53b24eda41
James Karnes <karnes.james@gmail.com>
2024-09-16 15:48:57 -0400

    started navbar

--- src/components/Layout/Header/LinksRight.js
+++ src/components/Layout/Header/LinksRight.js
@@ -1,30 +1,30 @@
-import React from 'react'
+import React from "react";
 import { Link } from "gatsby";
 const LinksRight = () => {
   return (
     <div className="hidden lg:flex lg:justify-between lg:w-[45vw]">
-        <Link to="/" className="no-underline">
+      <Link to="/" className="no-underline">
         <button className={`navLinks text-primary-color`} translate="no">
-        Inicio
+          Inicio
         </button>
       </Link>
       <Link to="/" className="no-underline">
         <button className={`navLinks text-primary-color`} translate="no">
-        Sobre Nosotros
+          Sobre Nosotros
         </button>
       </Link>
       <Link to="/" className="no-underline">
         <button className={`navLinks text-primary-color`} translate="no">
-        Propuestas
+          Propuestas
         </button>
       </Link>
       <Link to="/" className="no-underline">
         <button className={`navLinks text-primary-color`} translate="no">
-        Contacto
+          Contacto
         </button>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default LinksRight
+export default LinksRight;
