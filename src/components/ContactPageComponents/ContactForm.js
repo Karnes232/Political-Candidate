03d500130d49bce6d603077ba8d18d6ec1155881
James Karnes <karnes.james@gmail.com>
2024-09-19 19:04:54 -0400

    added cedula to form, and when autocomplete keeping it transparent

--- src/components/ContactPageComponents/ContactForm.js
+++ src/components/ContactPageComponents/ContactForm.js
@@ -13,10 +13,15 @@ const ContactForm = () => {
         data-netlify="true"
         data-netlify-honeypot="bot-field"
         id="contactForm"
-        className="w-4/5 md:w-full max-w-md flex flex-col justify-center items-center mx-auto my-5 bg-white p-5 rounded-lg"
+        className="w-4/5 md:w-full max-w-md flex flex-col justify-center items-center mx-auto my-5 bg-gray-900 bg-opacity-75 p-5 rounded-lg"
       >
         <input type="hidden" name="form-name" value="Contact Form" />
-        <ContactInfo name={name} setName={setName} lastName={lastName} setLastName={setLastName}/>
+        <ContactInfo
+          name={name}
+          setName={setName}
+          lastName={lastName}
+          setLastName={setLastName}
+        />
         <button
           type="submit"
           className="px-10 py-2 my-3 bg-primary-color text-white text-lg font-bold uppercase rounded hover:opacity-70 focus:outline-none focus:bg-gray-700"
--- src/components/ContactPageComponents/ContactInfo.js
+++ src/components/ContactPageComponents/ContactInfo.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 const ContactInfo = ({ name, setName, lastName, setLastName }) => {
+  const [idNumber, setIdNumber] = useState("");
   const handlePhoneChange = (e) => {
-    console.log(e)
+    console.log(e);
   };
+  const handleInputChange = (e) => {
+    let value = e.target.value.replace(/\D/g, ""); // Remove non-numeric characters
 
+    // Apply formatting as XXX-XXXXXXX-X
+    if (value.length > 3 && value.length <= 10) {
+      value = value.replace(/(\d{3})(\d{1,7})/, "$1-$2");
+    } else if (value.length > 10) {
+      value = value.replace(/(\d{3})(\d{7})(\d{1})/, "$1-$2-$3");
+    }
+
+    setIdNumber(value);
+  };
   return (
     <>
       <div className="relative z-0 mb-6 w-full group">
@@ -36,6 +48,22 @@ const ContactInfo = ({ name, setName, lastName, setLastName }) => {
         />
         <label htmlFor="apellido" className="contactFormLabel">
           Apellido
+        </label>
+      </div>
+      <div className="relative z-0 mb-6 w-full group">
+        <input
+          type="text"
+          name="cedula"
+          id="cedula"
+          value={idNumber}
+          onChange={handleInputChange}
+          maxLength={13}
+          className="contactFormInput peer"
+          placeholder=""
+          required
+        />
+        <label htmlFor="cedula" className="contactFormLabel">
+          Cedula
         </label>
       </div>
       <div className="relative z-0 mb-6 w-full group">
